7db79ff12587dcb0a1f7a3bf961cc10e3716dc0b
itoktsnhc <itoktsnhc@hotmail.com>
2022-06-20 18:00:01 +0800

    add basic

---
+++ t_mediatR/itok.MediatR/itok.MediatR/00.Basic.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itok.MediatR
+{
+    public class _00
+    {
+        public class ReqBasic : IRequest<RespBasic>
+        {
+            public string ReqPayload { get; set; }
+        }
+
+        public class RespBasic
+        {
+            public string RespPayload { get; set; }
+        }
+
+        public class BasicHandler : IRequestHandler<ReqBasic, RespBasic>
+        {
+            public Task<RespBasic> Handle(ReqBasic req, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(new RespBasic()
+                {
+
+                    RespPayload = $"mediatR with req's payload {req.ReqPayload}"
+                });
+            }
+        }
+
+    }
+}
---
+++ t_mediatR/itok.MediatR/itok.MediatR/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using static itok.MediatR._00;
+
+var sc = new ServiceCollection();
+
+sc.AddMediatR(typeof(Program));
+
+var sp = sc.BuildServiceProvider();
+
+#region Basic_Req_Resp
+
+var mediator = sp.GetRequiredService<IMediator>();
+var req = new ReqBasic() { ReqPayload = "PayLoad" };
+
+var resp = await mediator.Send(req);
+Console.WriteLine(resp.RespPayload);
+
+#endregion
