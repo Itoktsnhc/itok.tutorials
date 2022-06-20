79922aadf1b9f50bf8a59786607db59bb5509e64
itoktsnhc <itoktsnhc@hotmail.com>
2022-06-20 22:58:54 +0800

    update

--- t_mediatR/itok.MediatR/itok.MediatR/00.Basic.cs
+++ t_mediatR/itok.MediatR/itok.MediatR/00.Basic.cs
@@ -14,6 +14,16 @@ namespace itok.MediatR
             public string ReqPayload { get; set; }
         }
 
+        public class StreamReq : IStreamRequest<RespBasic>
+        {
+            public string Payload { get; set; }
+        }
+
+        public class NotifyReq : INotification
+        {
+            public string Payload { get; set; }
+        }
+
         public class RespBasic
         {
             public string RespPayload { get; set; }
@@ -28,6 +38,29 @@ namespace itok.MediatR
 
                     RespPayload = $"mediatR with req's payload {req.ReqPayload}"
                 });
+            }
+        }
+
+        public class StreamBasicHandler : IStreamRequestHandler<StreamReq, RespBasic>
+        {
+            public async IAsyncEnumerable<RespBasic> Handle(StreamReq request, CancellationToken cancellationToken)
+            {
+                foreach (var x in Enumerable.Range(0, 10))
+                {
+                    yield return await Task.FromResult(new RespBasic()
+                    {
+                        RespPayload = $"{request.Payload} + {x}"
+                    });
+                }
+            }
+        }
+
+        public class NotifyBasicHandler : INotificationHandler<NotifyReq>
+        {
+            public Task Handle(NotifyReq notification, CancellationToken cancellationToken)
+            {
+                Console.WriteLine($"Notification: {notification.Payload}");
+                return Task.CompletedTask;
             }
         }
 
--- t_mediatR/itok.MediatR/itok.MediatR/Program.cs
+++ t_mediatR/itok.MediatR/itok.MediatR/Program.cs
@@ -9,13 +9,35 @@ var sc = new ServiceCollection();
 sc.AddMediatR(typeof(Program));
 
 var sp = sc.BuildServiceProvider();
-
-#region Basic_Req_Resp
-
 var mediator = sp.GetRequiredService<IMediator>();
-var req = new ReqBasic() { ReqPayload = "PayLoad" };
+
+#region Basic:Req_Resp
+
+var req = new ReqBasic() { ReqPayload = "Payload" };
 
 var resp = await mediator.Send(req);
 Console.WriteLine(resp.RespPayload);
+
+#endregion
+
+Console.WriteLine("==============================");
+
+#region Streams and AsyncEnumerables
+var streamReq = new StreamReq { Payload = "streamPayload" };
+await foreach (var item in mediator.CreateStream(streamReq))
+{
+    Console.WriteLine(item.RespPayload);
+    await Task.Delay(1000);
+}
+
+#endregion
+
+Console.WriteLine("==============================");
+
+#region Notifications
+
+var notifyReq = new NotifyReq { Payload = "event 1" };
+
+await mediator.Publish(notifyReq);
 
 #endregion
