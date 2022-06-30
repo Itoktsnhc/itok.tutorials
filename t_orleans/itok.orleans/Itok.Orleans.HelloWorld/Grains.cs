6a52308fb603b4bf2b2d7a9fc7b796937da23369
itoktsnhc <itoktsnhc@hotmail.com>
2022-06-30 18:04:02 +0800

    update

---
+++ t_orleans/itok.orleans/Itok.Orleans.HelloWorld/Grains.cs
@@ -0,0 +1,16 @@
+﻿using Orleans;
+
+namespace Itok.Orleans.HelloWorld;
+
+public interface IFriendGrain : IGrainWithStringKey
+{
+    Task<string> SayHiAsync(string name);
+}
+
+public class FriendGrain : Grain, IFriendGrain
+{
+    public Task<string> SayHiAsync(string name)
+    {
+        return Task.FromResult($"Hi {name}, I am {this.GetPrimaryKeyString()}.");
+    }
+}
---
+++ t_orleans/itok.orleans/Itok.Orleans.HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using Itok.Orleans.HelloWorld;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Hosting;
+
+var host = new HostBuilder()
+    .UseOrleans(x => { x.UseLocalhostClustering(); })
+    .Build();
+
+await host.StartAsync();
+
+var grainFactory = host.Services.GetRequiredService<IGrainFactory>();
+
+var tom = grainFactory.GetGrain<IFriendGrain>("Tom");
+
+var resp = await tom.SayHiAsync("Jerry");
+
+Console.WriteLine(resp);
+Console.ReadLine();
